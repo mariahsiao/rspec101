31892675c49f29ea0ca60b779b72e4dafb99d4cf
mariahsiao <thatsjustmeiwant@gmail.com>
2016-10-08 13:49:10 +0800

    user need login then can do course#update

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
 
-  before_action :authenticate_user!, :only => [:new, :create, :edit]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 
   def index
     @courses = Course.all
